bfa1dd84e4713763715fc3dfd8ccb4d6d0254853
Eyadmohamed1332 <eyadzohier123@gmail.com>
2023-05-12 11:32:50 +0300

    HW 4,5 with some modifications

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import HomePage from "./Components/Pages/HomePage"
+import HomePage from "./components/pages/Index"
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import TasksPage from "./components/pages/Tasks";
+import Projects from "./components/pages/Projects";
 
 function App() {
   return (
       <div className="App">
-        <HomePage />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/project/:projectId/tasks" element={<TasksPage />} />
+            </Routes>
+        </BrowserRouter>
       </div>
   );
 }
